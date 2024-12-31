e1ac1ee222421ad2c95730306bdd8399712473a4
VasimShaikh1710 <vasimmrshaikh@gmil.com>
2024-12-31 14:28:33 +0530

    settings

--- phnproject/settings.py
+++ phnproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b5@q=wjf5$)58h1c$!-@%zdajuu5vp03++e9fpi7yk$ma5owfx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
